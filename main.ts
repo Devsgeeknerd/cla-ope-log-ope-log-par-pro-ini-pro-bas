50f4a73febcfc8001cd2db376949bf5e1bc65374
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-17 15:24:07 -0300

    atualizando conteudo

--- main.ts
+++ main.ts
@@ -2,28 +2,22 @@
 console.log(true && true);
 console.log(true && false);
 console.log(false && true);
-console.log(false && false); 
+console.log(false && false);
 
+// true se x estiver entre 18 e 90, caso contrário, false.
 let x: number = 50;
-console.log(x >= 18 && x <= 90); // true se x estiver entre 18 e 90, caso contrário, false
-
+console.log(x >= 18 && x <= 90);
 
 // Operador lógico OU ("||").
-console.log(true || true);   // true
-console.log(true || false);  // true
-console.log(false || true);  // true
-console.log(false || false); // false
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
 
+// true se y for menor que 10 ou maior que 20, caso contrário, false.
 let y: number = 50;
-console.log(y < 10 || y > 20); // true se y for menor que 10 ou maior que 20, caso contrário, false
+console.log(y < 10 || y > 20);
 
 // Operador lógico NÃO ("!").
-console.log(!true);  
-console.log(!false); 
-
-// Observações:
-// - O operador lógico NOT inverte o valor da expressão booleana.
-
-// Geral:
-// - Boas práticas incluem o uso de parênteses para garantir a ordem correta das operações lógicas.
-
+console.log(!true);
+console.log(!false);
